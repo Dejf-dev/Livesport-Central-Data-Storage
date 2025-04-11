ba82557c08f831d4e1efbb88be9ad8a451b3c499
David Ratimec <ratimec.david99@gmail.com>
2025-04-11 14:13:14 +0200

    update repositories with default find methods

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -8,11 +8,21 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Event>
+ *
+ * @method Event|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Event|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Event[]    findAll()
+ * @method Event[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class EventRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Event::class);
+    }
+
+    public function findById(int $id): ?Event
+    {
+        return $this->findOneBy(["id" => $id]);
     }
 }
--- src/Repository/FootballMatchRepository.php
+++ src/Repository/FootballMatchRepository.php
@@ -8,11 +8,21 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<FootballMatch>
+ *
+ * @method FootballMatch|null find($id, $lockMode = null, $lockVersion = null)
+ * @method FootballMatch|null findOneBy(array $criteria, array $orderBy = null)
+ * @method FootballMatch[]    findAll()
+ * @method FootballMatch[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class FootballMatchRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, FootballMatch::class);
+    }
+
+    public function findById(int $id): ?FootballMatch
+    {
+        return $this->findOneBy(["id" => $id]);
     }
 }
